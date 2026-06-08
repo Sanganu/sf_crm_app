3cfc03a0db01245ee2e9772b003e111701bee16a
Sanganu <kppkmsanu@gmail.com>
2026-06-07 22:19:59 -0600

    CSS file

--- UniversityCRM/force-app/main/default/lwc/enrollmentPanel/enrollmentPanel.js
+++ UniversityCRM/force-app/main/default/lwc/enrollmentPanel/enrollmentPanel.js
@@ -1,5 +1,6 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getEnrollments from '@salesforce/apex/EnrollmentService.getEnrollments';
 
-export default class EnrollmentPanel extends LightningElement{
-    
+export default class EnrollmentPanel extends LightningElement {
+    @wire(getEnrollments) enrollments;
 }
