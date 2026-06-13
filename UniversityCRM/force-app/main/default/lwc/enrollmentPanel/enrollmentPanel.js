5c2979303406341e3e7a04743dca18e08fc383dd
Sanganu <kppkmsanu@gmail.com>
2026-06-13 11:01:32 -0600

    Adding changes to enrollment service and student service and LWC

--- UniversityCRM/force-app/main/default/lwc/enrollmentPanel/enrollmentPanel.js
+++ UniversityCRM/force-app/main/default/lwc/enrollmentPanel/enrollmentPanel.js
@@ -2,5 +2,19 @@ import { LightningElement, wire } from 'lwc';
 import getEnrollments from '@salesforce/apex/EnrollmentService.getEnrollments';
 
 export default class EnrollmentPanel extends LightningElement {
-    @wire(getEnrollments) enrollments;
+    @api recordId;
+    wiredResult;
+    enrollments = [];
+    error;
+
+    @wire(getEnrollmentForStudents,{ studentId: '$recordId' })
+    wired(result){
+        this.wiredResult = result;
+        if(result.data){this.enrollments = result.data; this.error = undefined;}
+        else if(result.error){ this.error = result.error; this.enrollments=[]}
+    }
+
+    handleRefresh(){
+        return refreshApex(this.wiredResult);
+    }
 }
