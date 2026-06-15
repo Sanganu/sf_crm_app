e2e9e0fa669c022dda52e90f65d8923f55d035fd
Sanganu <kppkmsanu@gmail.com>
2026-06-14 23:16:09 -0600

    Changes

--- UniversityCRM/force-app/main/default/lwc/enrollmentPanel/enrollmentPanel.js
+++ UniversityCRM/force-app/main/default/lwc/enrollmentPanel/enrollmentPanel.js
@@ -2,19 +2,5 @@ import { LightningElement, wire } from 'lwc';
 import getEnrollments from '@salesforce/apex/EnrollmentService.getEnrollments';
 
 export default class EnrollmentPanel extends LightningElement {
-    @api recordId;
-    wiredResult;
-    enrollments = [];
-    error;
-
-    @wire(getEnrollmentForStudents,{ studentId: '$recordId' })
-    wired(result){
-        this.wiredResult = result;
-        if(result.data){this.enrollments = result.data; this.error = undefined;}
-        else if(result.error){ this.error = result.error; this.enrollments=[]}
-    }
-
-    handleRefresh(){
-        return refreshApex(this.wiredResult);
-    }
+    @wire(getEnrollments) enrollments;
 }
